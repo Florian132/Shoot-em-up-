8529f85d13991b3a455be9ddd70cb712ab54884c
Ratmir <foxratmir2000@gmail.com>
2020-02-13 16:23:20 +0100

    EnemyBullet script

---
+++ Starship 13/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float speed = -4f;
+    public Rigidbody2D rb;
+    public int damage = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    void Update()
+    {
+
+    }
+
+}
