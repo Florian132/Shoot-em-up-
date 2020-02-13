b03ec6fbb82fbaacff706eca9e1bf7fd518d6655
Elliot <elliot.padazakos@outlook.com>
2020-02-13 16:03:21 +0100

    Project upload
    Latest version 13-02-20

---
+++ Starship 13/Assets/Scripts/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    private Rigidbody2D rb;
+    public int enemySpeed = -2;
+    public GameObject BulletPrefab;
+    public float ShootingRate;
+
+
+    Highscore highscore;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        highscore = GameObject.FindObjectOfType<Highscore>();
+
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, enemySpeed);
+    }
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            
+            Die();
+        }
+    }
+    void Die()
+    {
+        highscore.addScore(100);
+        Destroy(gameObject);
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Deleter")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Shooting()
+    {
+        var ShotTransform = Instantiate(BulletPrefab);
+
+    }
+
+}
