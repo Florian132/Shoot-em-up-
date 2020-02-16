26b0df4001b918f1778c3bc22f6fbc043efc9c49
Ratmir <foxratmir2000@gmail.com>
2020-02-16 23:12:57 +0100

    Auto stash before merge of "master" and "origin/master"

--- Shmup test-1/Assets/Scripts/Enemy.cs
+++ Shmup test-1/Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -31,3 +32,38 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    private Rigidbody2D rb;
+    public int enemySpeed = -2;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, enemySpeed);
+    }
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+
+        Destroy(gameObject);
+    }
+}
+>>>>>>> Stashed changes
---
+++ Starship 13/Assets/Scripts/EnemyWeapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour
+{
+    GameObject bulletPrefab;
+    Transform firePoint;
+    public float fireRate = 10;
+    private float timer;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer < 1 / fireRate) return;
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        timer = 0;
+    }
+}
